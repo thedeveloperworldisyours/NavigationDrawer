5b55da3ad5aa24a78b4cb4fb1a4f7ddecc5a7680
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-01-23 12:20:47 +0000

    update

---
+++ app/src/main/java/com/thedeveloperworldisyours/navigationdrawer/fragments/FirstFragment.java
@@ -0,0 +1,33 @@
+package com.thedeveloperworldisyours.navigationdrawer.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thedeveloperworldisyours.navigationdrawer.R;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    public static FirstFragment newInstance() {
+        return new FirstFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/thedeveloperworldisyours/navigationdrawer/fragments/SecondFragment.java
@@ -0,0 +1,33 @@
+package com.thedeveloperworldisyours.navigationdrawer.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.thedeveloperworldisyours.navigationdrawer.R;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    public static SecondFragment newInstance() {
+        return new SecondFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+}
